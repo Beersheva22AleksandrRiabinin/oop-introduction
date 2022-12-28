a9a7e86707095492f8f56e6d61354de853d60adf
AleksandrRiabinin <alexr2141@gmail.com>
2022-12-28 18:11:24 +0200

    CW # 17 + HW # 18

--- src/telran/util/List.java
+++ src/telran/util/List.java
@@ -3,11 +3,11 @@ package telran.util;
 public interface List<T> extends Collection<T> {
 	
 	void add(int index, T element);
-	T remove(int index); //в случае неправильного индекса бросается эксепшн
-	int indexOf(T pattern); // -1 если нету такого
-	int lastIndexOf(T pattern); //говорит писали такое уже
+	T remove(int index);
+	int indexOf(T pattern);
+	int lastIndexOf(T pattern);
 	T get(int index);
-	void set(int index, T element); //изменяет элемент под индексом
+	void set(int index, T element);
 	
 
 }
